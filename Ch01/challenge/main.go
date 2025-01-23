5f1aaf65a2acd9c5975252d87cb3d0a5e9281450
JosephGladwinGeorge <jgladwin27.2002@gmail.com>
2025-01-23 10:57:39 +0530

    solved first challenge

--- Ch01/challenge/main.go
+++ Ch01/challenge/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
 
 /*
 Instructions:
@@ -15,8 +21,100 @@ Instructions:
 - Use -out to specify output file
   - Default to stdout
 */
+
+type Width struct {
+  val *int
+}
+
+func (w *Width) String() string {
+  if w.val == nil {
+    return ""
+  }
+
+  return fmt.Sprintf("%d", *w.val)
+}
+
+func (w *Width) Set(v string) error {
+  n,err := strconv.Atoi(v)
+  if err != nil {
+    return fmt.Errorf("bad number: %s", err)
+  }
+
+  if n<=0 || n>= 250{
+    return fmt.Errorf("width must be between 0 and 250")
+  }
+
+  *w.val=n
+
+  return nil
+}
+
+type out struct {
+  val *io.Writer
+  file *os.File
+}
+
+func (o *out) String() string {
+  if o.val == nil {
+    return ""
+  }
+  if o.file != nil {
+		return o.file.Name()
+	}
+
+  return fmt.Sprintf("%d", *o.val)
+}
+
+func (o *out) Set(v string) error {
+  if v== ""{
+    *o.val=os.Stdout
+    return nil
+  }
+  file,err:=os.OpenFile(v,os.O_RDWR|os.O_CREATE, 0644)
+  if err !=nil{
+    return fmt.Errorf("could not open file %s error:%s",v,err)
+  }
+
+  *o.val=file
+  o.file=file
+
+  return nil
+}
+
+func (o *out) Close()  {
+  if o.file != nil{
+    o.file.Close()
+  }
+}
+
+var config struct {
+  width int
+  out  io.Writer
+}
 func main() {
-	text := "Go"
-	width := 6
-	Banner(os.Stdout, text, width)
+  config.width=80
+  config.out=os.Stdout
+  outFlag := &out{val: &config.out}
+  flag.Var(&Width{&config.width},"width","width of banner default 80")
+  flag.Var(outFlag,"output", "output file to print to")
+  flag.Parse()
+
+  defer outFlag.Close()
+
+  text:=""
+  switch flag.NArg() {
+	case 0:
+		d,err:=io.ReadAll(os.Stdin)
+    if err !=nil {
+      fmt.Fprintf(os.Stderr, "error: can't read - %s\n", err)
+			os.Exit(1)
+    }
+    text=string(d)
+	case 1:
+		text = flag.Arg(0)
+	default:
+		fmt.Fprintln(os.Stderr, "error: wrong number of arguments")
+		os.Exit(1)
+	}
+	Banner(config.out, text, config.width)
 }
